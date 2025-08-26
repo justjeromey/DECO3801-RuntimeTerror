65d32c0bc2d4a695412b108824bd1c8ddf5b0942
Dan <danfuller1414@gmail.com>
2025-08-26 17:34:31 +1000

    got rid of top down graph and some unnecessary print statements

--- backend/gpx_turning_points.py
+++ backend/gpx_turning_points.py
@@ -11,16 +11,6 @@ gpx_path = get_trail_file_path(trail_name)
 
 gpx_result: GPXData = parse_gpx(gpx_path)
 
-
-print("elevations expressed as a list")
-print(gpx_result.elevations)
-print("distance along trail")
-print(gpx_result.cumulative_distances_m)
-
-
-
-
-
 first = 0
 second = 0
 grad = "neutral"
@@ -42,30 +32,13 @@ for y in gpx_result.elevations:
 
 print(index_list)
 
-
 turning_x = []
 turning_y = []
 for index in index_list:
     turning_x.append(gpx_result.cumulative_distances_m[index-1] / 1000)
     turning_y.append(gpx_result.elevations[index-1])
 
-
-
-
 num_points = len(gpx_result.latitudes)
-
-trail_markersize = max(0.5, 20 / num_points)  # Adjust 20 to tune overall size
-print("Trail marker size: ", trail_markersize)
-## Plotting the trail topdown view
-plt.figure(figsize=(8, 6))
-plt.plot(gpx_result.longitudes, gpx_result.latitudes, marker='o', linestyle='-', color='blue', markersize=trail_markersize)
-plt.title('GPX Trail Visualization')
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.grid(True)
-plt.gca().set_aspect('equal', adjustable='box') 
-plt.show(block=False)
-
 
 fig, ax = plt.subplots(figsize=(10, 4))
 ax.plot(gpx_result.convert_distance_to_km, gpx_result.elevations, marker='.', color='green')

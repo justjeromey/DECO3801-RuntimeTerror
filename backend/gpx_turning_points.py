7d96a5b7c0b43d80460f67572f9b0083dda8cc88
Dan <danfuller1414@gmail.com>
2025-08-26 17:31:45 +1000

    Fixed algorithm. Turning points are detected now.

--- backend/gpx_turning_points.py
+++ backend/gpx_turning_points.py
@@ -46,8 +46,8 @@ print(index_list)
 turning_x = []
 turning_y = []
 for index in index_list:
-    turning_x.append(gpx_result.cumulative_distances_m[index] / 1000)
-    turning_y.append(gpx_result.elevations[index])
+    turning_x.append(gpx_result.cumulative_distances_m[index-1] / 1000)
+    turning_y.append(gpx_result.elevations[index-1])
 
 
 

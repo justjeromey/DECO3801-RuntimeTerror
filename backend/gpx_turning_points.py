c09dc61cd50d5c395d0f86fd8042d597abf9b28f
Zach <s4800832@student.uq.edu.au>
2025-08-31 23:25:29 +1000

    Added minor comments and threshold const

--- backend/gpx_turning_points.py
+++ backend/gpx_turning_points.py
@@ -3,53 +3,62 @@ from matplotlib.ticker import MultipleLocator
 
 from parseGpx import get_trail_file_path, parse_gpx, GPXData
 
+ROLLING_SEGMENT_THRESHOLD = 200  # Meters
 
 
-trail_name = "fells_loop"
+trail_name = "iphone"
 print(f"Loading {trail_name} GPX data...")
 gpx_path = get_trail_file_path(trail_name)
 
 gpx_result: GPXData = parse_gpx(gpx_path)
 
 
-#find turning points 
+# Find turning points 
 first = 0
 second = 0
 grad = "neutral"
 index_list = []
-i=0
+i = 0
+
 for y in gpx_result.elevations: 
     second = first
     first = y
     diff = first - second 
+
+    # Uphill
     if diff > 0 :
         if grad != "pos":
             index_list.append(i)
         grad = "pos"
+    # Downhill
     elif diff < 0:
         if grad != "neg":
             index_list.append(i) 
         grad = "neg"
+
     i += 1
 
 print(index_list)
 
+# Collect x and y of turning points
 turning_x = []
 turning_y = []
 for index in index_list:
+    # Converting m to km
     turning_x.append(gpx_result.cumulative_distances_m[index-1] / 1000)
     turning_y.append(gpx_result.elevations[index-1])
 
-#calculate hypotenuse 
-threshold = 200
+# Calculate hypotenuse
 rolling_x = []
 rolling_y = []
 
-for x in range(len(turning_x)-1):
-    x_diff = (turning_x[x+1] - turning_x[x])*1000
+for x in range(len(turning_x) - 1):
+    x_diff = (turning_x[x+1] - turning_x[x]) * 1000
     y_diff = abs(turning_y[x+1] - turning_y[x])
     hypotenuse = (x_diff**2 + y_diff**2)**0.5
-    if hypotenuse < threshold:
+
+    # If section is short then it is rolling hill
+    if hypotenuse < ROLLING_SEGMENT_THRESHOLD:
         rolling_x.extend([turning_x[x],turning_x[x+1]])
         rolling_y.extend([turning_y[x], turning_y[x+1]])
         print(hypotenuse)
@@ -58,8 +67,9 @@ num_points = len(gpx_result.latitudes)
 
 fig, ax = plt.subplots(figsize=(10, 4))
 ax.plot(gpx_result.convert_distance_to_km, gpx_result.elevations, marker='.', color='green')
-ax.scatter(turning_x, turning_y, c="blue", marker="o", s=100, alpha=0.7, edgecolors="black")
-ax.scatter(rolling_x, rolling_y, c="red", marker="o", s=100, alpha=0.7, edgecolors="black")
+ax.scatter(turning_x, turning_y, c="blue", label="Turning points", marker="o", s=100, alpha=0.7, edgecolors="black")
+ax.scatter(rolling_x, rolling_y, c="red", label="Rolling Hills", marker="o", s=100, alpha=0.7, edgecolors="black")
+ax.legend()
 ax.set_title('Elevation Profile')
 ax.set_xlabel('Distance (km)')
 ax.set_ylabel('Elevation (m)')

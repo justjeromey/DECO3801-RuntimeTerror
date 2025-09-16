4910dfb315f6c3652ec6deb2d36c43ea994a4f19
Dan <danfuller1414@gmail.com>
2025-09-16 14:08:53 +1000

    added file based on main which will send stats as well as the gpx file

---
+++ backend/stats_api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from parseGpx import parse_gpx, convert_gpx_data_to_json
+from gpx_turning_points import generateStats, convertStatsToJSON, calculateSegmentStats
+
+app = FastAPI()
+
+@app.post("/format-gpx")
+async def upload_gpx(file: UploadFile = File(...)):
+    #check its gpx
+    if not file.filename.endswith(".gpx"):
+        return JSONResponse(status_code=400, content={"message": "Invalid file type. Please upload a GPX file."})
+
+    # Process the GPX file
+    json = generateStats(file.filename)
+
+
+    return JSONResponse(status_code=200, content=json)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
